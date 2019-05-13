0096558f67d8db280048579e486002867fbd333c
Gabriel Conzuelo <gabrielconzuelo@Gabriels-MacBook-Pro.local>
2019-05-13 19:43:00 -0400

    R code modification to auto TMLE AIPW

--- R/simulation.R
+++ R/simulation.R
@@ -15,11 +15,9 @@ for (package in packages) {
   library(package, character.only=T)
 }
 
-devtools::install_github("ecpolley/SuperLearner")
 library(SuperLearner)
-
-install.packages("tmle",repos='http://lib.stat.cmu.edu/R/CRAN')
 library(tmle)
+
 
 time<-proc.time()
 expit <- function(x){ exp(x)/(1+exp(x)) }
@@ -49,18 +47,18 @@ interaction_sim<-function(counter){
   n<-1500
   p<-2
   cat("Now running iteration",i,"with a sample size of",n,'\n');flush.console()
-
+  
   # confounders
   sigma<-matrix(0,nrow=p,ncol=p);diag(sigma)<-1
   c <- rmvnorm(n, mean=rep(0,p), sigma=sigma)
   
   # design matrix for outcome model
-
+  
   muMatT<-model.matrix(as.formula(paste("~(",paste("c[,",1:ncol(c),"]",collapse="+"),")")))
-
+  
   parms3<-c(3,3)
   parms4<-c(log(1.5),log(1.5))
-
+  
   beta<-parms3;beta<-c(120,beta)
   # design matrix for propensity score model
   piMatT<-model.matrix(as.formula(paste("~(",paste("c[,",1:ncol(c),"]",collapse="+"),")")))
@@ -75,71 +73,66 @@ interaction_sim<-function(counter){
   m<-rbinom(n,1,pi_m)
   # outcome model: true expsoure effect = 6
   y <- x*6 + m*6 - 3*x*m + mu + rnorm(n,0,6)
-
+  
   # correct specification
   dat <- data.frame(c,m,x,y); colnames(dat)[1:ncol(c)] <- paste("c",1:ncol(c),sep="")
-
+  
   C <- muMatT[,-1];colnames(C) <- paste("C",1:ncol(muMatT[,-1]), sep="");
   X <- x
   M <- m
   Y <- y
-
+  
   C0 <- subset(C,M==0)
   C1 <- subset(C,M==1)
-
+  
   X0 <- subset(X,M==0)
   X1 <- subset(X,M==1)
-
+  
   Y0 <- subset(Y,M==0)
   Y1 <- subset(Y,M==1)
-
+  
   mod1 <- lm(y~x+m+x*m+c1+c2,data=dat)
-
-
+  
   mod2_0 <- lm(y~x+c1+c2,data=subset(dat,m==0))
   mod2_1 <- lm(y~x+c1+c2,data=subset(dat,m==1))
-
+  
   folds<-c(10)
   cat("Number of cross-validation folds is",folds,'\n');flush.console()
   tmle_strat0 <- tmle(Y0,X0,C0,family="gaussian",
                       Q.SL.library=sl.lib_mu,g.SL.library=sl.lib_pi,
                       V=folds)
-
+  
   tmle_strat1 <- tmle(Y1,X1,C1,family="gaussian",
                       Q.SL.library=sl.lib_mu,g.SL.library=sl.lib_pi,
                       V=folds)
-
+  
   # ps.sl.coefNPT <- cbind(n,t(tmleNPT$g$coef))
   # mu.sl.coefNPT <- cbind(n,t(tmleNPT$Qinit$coef))
-
+  
   pihat_0 <- tmle_strat0$g$g1W
   pihat_1 <- tmle_strat1$g$g1W
-
+  
   pihat_0 <- ifelse(pihat_0 < 0.025,0.025,pihat_0)
   pihat_0 <- ifelse(pihat_0 > 1-0.025,1-0.025,pihat_0)
-
+  
   pihat_1 <- ifelse(pihat_1 < 0.025,0.025,pihat_1)
   pihat_1 <- ifelse(pihat_1 > 1-0.025,1-0.025,pihat_1)
-
+  
   muhat_0  <- tmle_strat0$Qinit$Q[,2]*X0+tmle_strat0$Qinit$Q[,1]*(1-X0)
   muhat_1  <- tmle_strat1$Qinit$Q[,2]*X1+tmle_strat1$Qinit$Q[,1]*(1-X1)
-
+  
   muhat1_0  <- tmle_strat0$Qinit$Q[,2]
   muhat1_1  <- tmle_strat1$Qinit$Q[,2]
-
+  
   muhat0_0  <- tmle_strat0$Qinit$Q[,1]
   muhat0_1  <- tmle_strat1$Qinit$Q[,1]
   
-  ## for the "auto" versions
-  #### step 1: fit the estimators in overall data
-  
+  ## Auto versions
   #### TMLE version
   Z <- cbind(M,C)
   tmle_res <- tmle(Y,X,Z,family="gaussian",
-                      Q.SL.library=sl.lib_mu,g.SL.library=sl.lib_pi,
-                      V=folds)
-  
-  tmle_EFF <- tmle_res$Qstar[,2] - tmle_res$Qstar[,1]
+                   Q.SL.library=sl.lib_mu,g.SL.library=sl.lib_pi,
+                   V=folds)
   
   pihat <- tmle_res$g$g1W
   pihat <- ifelse(pihat < 0.025,0.025,pihat)
@@ -148,7 +141,8 @@ interaction_sim<-function(counter){
   muhat  <- tmle_res$Qinit$Q[,2]*X+tmle_res$Qinit$Q[,1]*(1-X)
   muhat1  <- tmle_res$Qinit$Q[,2]
   muhat0  <- tmle_res$Qinit$Q[,1]
-
+  
+  tmle_EFF <- tmle_res$Qstar[,2] - tmle_res$Qstar[,1]
   aipw_EFF <- as.numeric((((2*X-1)*(Y - muhat))/((2*X-1)*pihat + (1-X)) + muhat1 - muhat0)) ## inside the mean is EIE_aipw
   
   ## we have to figure out what the best way to do this is:
@@ -166,72 +160,72 @@ interaction_sim<-function(counter){
   mu_aipw_eff0 <- predict(ranger_aipw_eff,data=np_dat0)$pred
   mu_aipw_eff1 <- predict(ranger_aipw_eff,data=np_dat1)$pred
   
-  mean(mu_tmle_eff0)
-  mean(mu_tmle_eff1)
   
-  mean(mu_aipw_eff0)
-  mean(mu_aipw_eff1)
-  
-
-  
-  
-  
-  
-
-
   # compute estimators
   res.est$glm0[i] <- coef(mod1)["x"]
   res.est$glm1[i] <- coef(mod1)["x"] + coef(mod1)["x:m"]
-
+  
   res.est$glm_strat0[i] <- coef(mod2_0)["x"]
   res.est$glm_strat1[i] <- coef(mod2_1)["x"]
-
+  
   lower_bound <- min(y) - max(y)
   upper_bound <- max(y) - min(y)
-
+  
   aipw_strat0 <- mean((((2*X0-1)*(Y0 - muhat_0))/((2*X0-1)*pihat_0 + (1-X0)) + muhat1_0 - muhat0_0)) ## inside the mean is EIE_aipw
   aipw_strat1 <- mean((((2*X1-1)*(Y1 - muhat_1))/((2*X1-1)*pihat_1 + (1-X1)) + muhat1_1 - muhat0_1))
   aipw_strat0 <- ifelse(aipw_strat0>upper_bound,upper_bound,aipw_strat0)
   aipw_strat0 <- ifelse(aipw_strat0<lower_bound,lower_bound,aipw_strat0)
-
+  
   aipw_strat1 <- ifelse(aipw_strat1>upper_bound,upper_bound,aipw_strat1)
   aipw_strat1 <- ifelse(aipw_strat1<lower_bound,lower_bound,aipw_strat1)
-
+  
   res.est$AIPW_strat0[i]  <- aipw_strat0
   res.est$AIPW_strat1[i]  <- aipw_strat1
-
+  
   res.est$TMLE_strat0[i]<-tmle_strat0$estimates$ATE$psi
   res.est$TMLE_strat1[i]<-tmle_strat1$estimates$ATE$psi
-
+  
+  res.est$AIPW_auto0[i] <- mean(mu_aipw_eff0)
+  res.est$AIPW_auto1[i] <- mean(mu_aipw_eff1)
+  
+  res.est$TMLE_auto0[i] <- mean(mu_tmle_eff0)
+  res.est$TMLE_auto1[i] <- mean(mu_tmle_eff1)
+  
+  
   # compute closed-form SEs
   res.se$glm0[i] <- sqrt(vcov(mod1)["x","x"])
   res.se$glm1[i] <- sqrt(vcov(mod1)["x","x"] + vcov(mod1)["m","m"] - 2*vcov(mod1)["x","m"])
-
+  
   res.se$glm_strat0[i] <- sqrt(vcov(mod2_0)["x","x"])
   res.se$glm_strat1[i] <- sqrt(vcov(mod2_1)["x","x"])
-
+  
   aipw_strat0_se <- sd((((2*X0-1)*(Y0 - muhat_0))/((2*X0-1)*pihat_0 + (1-X0)) + muhat1_0 - muhat0_0))/sqrt(nrow(C0))
   aipw_strat1_se <- sd((((2*X1-1)*(Y1 - muhat_1))/((2*X1-1)*pihat_1 + (1-X1)) + muhat1_1 - muhat0_1))/sqrt(nrow(C1))
-
+  
   res.se$AIPW_strat0[i]  <- aipw_strat0_se
   res.se$AIPW_strat1[i]  <- aipw_strat1_se
-
+  
   res.se$TMLE_strat0[i] <- sqrt(tmle_strat0$estimates$ATE$var.psi)
   res.se$TMLE_strat1[i] <- sqrt(tmle_strat1$estimates$ATE$var.psi)
-
-
+  
+  ### AUTO TMLE / AIPW SEs
+  
+  
   # print updating results
-
+  
   res.est0 <- res.est %>% select(ends_with("0"))
   res.est1 <- res.est %>% select(ends_with("1"))
-
+  
   res.se0 <- res.se %>% select(ends_with("0"))
   res.se1 <- res.se %>% select(ends_with("1"))
-
+  
   res.cov0 <- res.est0-1.96*res.se0 < true1 & true1 < res.est0+1.96*res.se0
   res.cov1 <- res.est1-1.96*res.se1 < true2 & true2 < res.est1+1.96*res.se1
-
+  
+  ### AUTO TMLE / AIPW Cov
+  
   res.width <- (res.est+1.96*res.se) - (res.est-1.96*res.se)
+  
   tmp <- data.frame(rbind(c(n,apply(res.est0-true1,2,mean,na.rm=T),apply(res.est1-true2,2,mean,na.rm=T)),
                           c(n,apply((res.est0-true1)^2,2,mean,na.rm=T),apply((res.est1-true2)^2,2,mean,na.rm=T)),
                           c(n,apply(res.cov0,2,mean,na.rm=T),apply(res.cov1,2,mean,na.rm=T)),
@@ -240,7 +234,7 @@ interaction_sim<-function(counter){
   rownames(tmp)<-c("bias","rmse","cov","width");colnames(tmp)[1]<-"N";print(round(tmp,3));cat('\n');flush.console()
   colnames(tmp.se)[1]<-"N"
   setDT(tmp, keep.rownames = TRUE)[];colnames(tmp)[1] <- "type"
-
+  
   if(i==1){
     write.table(tmp,"./output/results.txt",sep="\t",row.names=F)
     write.table(tmp.se,"./output/results_se.txt",sep="\t",row.names=F)
@@ -251,7 +245,7 @@ interaction_sim<-function(counter){
   return(tmp)
 }
 
-interaction_sim(3)
+interaction_sim(10)
 
 start_time <- Sys.time()
 cores<-detectCores()-2
